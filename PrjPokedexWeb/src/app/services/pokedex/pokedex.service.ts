2fe9335d8aaead8c3b71c2d567804b7313f8d3ef
Jonas Eduardo de Almeida Pinheiro <jonas123.aluno@gmail.com>
2023-03-25 23:46:30 +0000

    FT001-J: Criação de rota get para o serviço 'pokedex' e importação do modulo 'HttpClientModule'

--- PrjPokedexWeb/src/app/services/pokedex/pokedex.service.ts
+++ PrjPokedexWeb/src/app/services/pokedex/pokedex.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokedexService {
+  private readonly URL = "https://pokeapi.co/api/v2/"
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  buscarApiPokedex(): Observable<any>{
+    return this.http.get<any>(`${URL}`)
+  }
 }
