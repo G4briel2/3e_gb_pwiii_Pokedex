66d1475057f625c524a9883cc5a99e185c5168af
Jonas Eduardo de Almeida Pinheiro <jonas123.aluno@gmail.com>
2023-04-03 11:26:33 +0000

    FT001-J: Criação de rotas para busca de pokemon por seu tipo e geraçaão e integração com a home

--- PrjPokedexWeb/src/app/components/home/home.component.ts
+++ PrjPokedexWeb/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { combineLatest, Observable } from 'rxjs';
 import { PokedexService } from 'src/app/services/pokedex/pokedex.service';
 
 @Component({
@@ -6,22 +7,51 @@ import { PokedexService } from 'src/app/services/pokedex/pokedex.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent{
+  pokemons: any[] = []
+  tipos: any[] = []
 
   constructor(
-    private pokedexService: PokedexService
-  ){
-    this.buscarRotas()
+    private pokedexService: PokedexService 
+  ){}
+
+  ngOnInit(): void{}
+
+  buscarPokemonPorGeracao(idGeracao: any): void{
+    this.pokedexService.buscarGeracao(idGeracao).subscribe(dados => {
+      const pokemons = dados.pokemon_species
+
+      const requisicoes = pokemons.map((pokemon: any) => {
+        const idPokemon = pokemon.url.split("/").at(-2)
+        return this.pokedexService.buscarPokemon(idPokemon)
+      })
+      combineLatest(requisicoes).subscribe({
+        next: (data: any) => {
+          this.pokemons = data
+        },
+        error: (erro) => {
+          console.error(erro)
+        }
+      })
+    })
   }
 
-  buscarRotas(){
-    this.pokedexService.buscarApiPokedex().subscribe({
-      next: (resposta) => {
-        console.log(resposta)
-      },
-      error: (erro) => {
-        console.log(erro)
-      }
+  buscarPokemonPorTipo(idTipo: any): void {
+    this.pokedexService.buscarTipo(idTipo).subscribe(dados => {
+      const pokemons = dados.pokemon
+
+      const requisicoes = pokemons.map((pokemon: any) => {
+        const idPokemon = pokemon.pokemon.url.split("/").at(-2)
+        return this.pokedexService.buscarPokemon(idPokemon)
+      })
+      combineLatest(requisicoes).subscribe({
+        next: (data: any) => {
+          this.tipos = data
+        },
+        error: (erro: any) => {
+          console.error(erro)
+        }
+      })
     })
   }
 }
--- PrjPokedexWeb/src/app/services/pokedex/pokedex.service.ts
+++ PrjPokedexWeb/src/app/services/pokedex/pokedex.service.ts
@@ -12,7 +12,15 @@ export class PokedexService {
     private http: HttpClient
   ) { }
 
-  buscarApiPokedex(): Observable<any>{
-    return this.http.get<any>(`${this.URL}pokemon`)
+  buscarGeracao(idGeracao: string): Observable<any>{
+    return this.http.get<any>(`${this.URL}generation/${idGeracao}`)
+  }
+
+  buscarPokemon(idPokemon: string): Observable<any>{
+    return this.http.get<any>(`${this.URL}pokemon/${idPokemon}`)
+  }
+
+  buscarTipo(idTipo: string): Observable<any>{
+    return this.http.get<any>(`${this.URL}type/${idTipo}`)
   }
 }
